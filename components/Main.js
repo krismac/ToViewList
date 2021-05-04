d91b705856574747deed848f7e7524f2d449fb39
Kris Mac <email@krismac.com>
2021-05-04 12:59:18 +0100

    first code added

---
+++ components/Main.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native';
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchUser } from '../redux/actions/index'
+
+
+export class Main extends Component {
+    componentDidMount() {
+    // this.props.clearData();
+    this.props.fetchUser();
+    // this.props.fetchUserPosts();
+    // this.props.fetchUserFollowing();
+}
+    
+
+    render() {
+        return (
+           <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Text>User is logged in</Text>
+          </View>
+        )
+    }
+}
+
+
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+fetchUser, dispatch
+})
+
+export default connect(null, mapDispatchToProps)(Main)
